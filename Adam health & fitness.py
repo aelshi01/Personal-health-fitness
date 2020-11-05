7a687fff2a694ddc367896cd63ef047d646453d1
aelshi01 <56831995+aelshi01@users.noreply.github.com>
2020-11-05 17:57:00 +0000

    initial commit
    Uploaded data collected from myfitnesspal app and a py file for retrieving data in a panda dataframe.

---
+++ Adam health & fitness.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 17:40:02 2020
+
+@author: student
+"""
+
+
+import pandas as pd
+
+
+data_cal = pd.read_csv('/Users/student/Desktop/Nutrition-Summary-2020-01-01-to-2020-11-04.csv')
+
+
+
+data_weight = pd.read_csv('/Users/student/Desktop/Measurement-Summary-2020-01-01-to-2020-11-04.csv')
+
+
