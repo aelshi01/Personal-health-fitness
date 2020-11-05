82ef54e7f31a6750b37507d88349250520f9773c
aelshi01 <56831995+aelshi01@users.noreply.github.com>
2020-11-05 18:46:56 +0000

    created dictionary
    dictionary with keys as dates and values as sum of calories for the day

--- Adam health & fitness.py
+++ Adam health & fitness.py
@@ -11,9 +11,18 @@ import pandas as pd
 
 
 data_cal = pd.read_csv('/Users/student/Desktop/Nutrition-Summary-2020-01-01-to-2020-11-04.csv')
-
-
-
 data_weight = pd.read_csv('/Users/student/Desktop/Measurement-Summary-2020-01-01-to-2020-11-04.csv')
 
+# 
 
+dic = dict()
+for i in range(0,len(data_cal['Date'])-1):
+    if data_cal['Date'][i] == data_cal['Date'][i+1]:
+        if data_cal['Date'][i] not in dic.keys():
+            dic[data_cal['Date'][i]] = int(data_cal['Calories'][i]) + int(data_cal['Calories'][i+1])
+            
+        else:
+            dic[data_cal['Date'][i]] += int(data_cal['Calories'][i+1])
+    
+    elif data_cal['Date'][i] not in dic.keys():
+        dic[data_cal['Date'][i]] = int(data_cal['Calories'][i])
